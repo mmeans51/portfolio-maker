04227c1e857e21955b460bbff45c289c311b8e5e
michael Means <colemeans51@gmail.com>
2022-01-10 12:32:07 -0600

    setting array

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
 const inquirer = require("inquirer");
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
       message: "What is your name?",
     },
-  ])
-  .then((answers) => console.log(answers));
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+  promptUser().then(answers => console.log(answers));
+  const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+      portfolioData.projects = [];
+    }
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+    });
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'what is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'decription',
+        message: 'provide a description of the project (required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'what did you build this project with? (check all that apply)',
+        choices: ['javascript', 'html','es6', 'jquery', 'boostrap', 'node']
+      },
+      {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'would you like to enter another project?',
+        default: false
+      }
+    ]);
+  };
+  promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 // const pageHTML = generatePage(fullName, githubName);
